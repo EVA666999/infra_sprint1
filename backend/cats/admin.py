f6f1739369ab68a48f4a0a2304100259a9bb5bae
EVA666999 <ukratitelkisok9913@inbox.ru>
2023-06-23 13:09:05 +0000

    fix_admin

--- backend/cats/admin.py
+++ backend/cats/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from models import Achievement, AchievementCat, Cat
+from .models import Achievement, AchievementCat, Cat
 
 class AchievementAdmin(admin.ModelAdmin):
     list_display = ('name',)
